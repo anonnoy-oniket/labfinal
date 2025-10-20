477b9bb26c7e1dd0ce3d7cb7a0140cafa1d8ce45
Anonnoy Oniket <70757271+anonnoy-oniket@users.noreply.github.com>
2025-10-20 07:19:53 +0600

    Add quicksort implementation in quicksort.cpp
    Implement quicksort algorithm with partitioning.

---
+++ quicksort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) swap(arr[++i], arr[j]);
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int &x : arr) cin >> x;
+
+    quickSort(arr, 0, n - 1);
+
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+    return 0;
+}
